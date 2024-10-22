c0592a520fb5ae34d2481dc423a9dfc6890db966
DucThinh <2251120054@ut.edu.vn>
2024-10-22 19:37:22 +0700

    Update Index and User

--- user.php
+++ user.php
@@ -6,7 +6,8 @@ if (!isset($_SESSION['userEmail'])) { //nếu session username k tồn tại th�
 }
 
 //truy vấn dữ liệu
-$city = "SELECT * FROM city";
+$city = "SELECT city.id, city.name, city.image, COUNT(hotel.id) AS total_hotels
+        FROM city LEFT JOIN hotel ON city.id = hotel.id_city GROUP BY city.id, city.name, city.image";
 $cityData = $mysqli->query($city);
 
 $hotel = "SELECT * FROM hotel";
@@ -148,18 +149,10 @@ $rowUser = $userName->fetch_assoc();
                         <a href="#">
                             <img src="<?php echo "./images/kham-pha-vn/" . $rowCity['image'] ?>" />
                             <h3> <?php echo $rowCity['name'] ?></h3>
-                            <?php $countHotel = 0; ?>
-                            <?php while ($rowHotel = $hotelData->fetch_assoc()) { ?>
-                                <?php if ($rowHotel['id_city'] == $rowCity['id']) { ?>
-                                    <?php $countHotel = $countHotel + 1; ?>
-                                <?php } ?>
-                            <?php } ?>
-                            <p><?php echo $countHotel . " chỗ nghỉ" ?></p>
+                            <p><?php echo $rowCity['total_hotels'] . " chỗ nghỉ" ?></p>
                         </a>
                     </div>
                 <?php } ?>
-
-
             </div>
             <button class="nav-button right" onclick="scrollCarousel(1)">
                 &#8250;
